7212da1b7e3eab9c109b672e86cfb53cf821e241
RitaliJ <ritaliaarav@gmail.com>
2024-08-10 22:20:02 -0700

    implemented approximate sat solver counter and fixed rand glue generation

--- random-glues-generator.py
+++ random-glues-generator.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 # Generate random 4-bit binary numbers for 25 rows and 2 columns
-random_binary_matrix = np.random.randint(0, 2, (25, 2, 4))
+random_binary_matrix = np.random.randint(0, 2, (25, 4, 4))
 
 # Number of binary numbers per position
-tpp = 2
+tpp = 4
 glues_p_tile = 4
 num_tiles = 25
 
 binary_string_matrix = []
 for i in range(num_tiles):
     row = set()
-    while len(row) != 2:
+    while len(row) != 4:
         elem = np.random.randint(0, 2, glues_p_tile)
         str_elem = "".join(map(str, elem))
         row.add(str_elem)
